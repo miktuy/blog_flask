25586f843b30b298040b7826f5cde7a0f4d9b1cd
miktuy <miktuy@mail.ru>
2020-05-11 21:03:52 +0300

    Added migration mechanics

--- app/models.py
+++ app/models.py
@@ -26,3 +26,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post id: {self.id}, title: {self.title}>'
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return f'<Tag id: {self.id}, name: {self.name}>'
