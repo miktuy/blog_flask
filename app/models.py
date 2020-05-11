cac50fffc61770acc4388adb43ac147ceafb996a
miktuy <miktuy@mail.ru>
2020-05-11 22:03:20 +0300

    Added many-to-many relations

--- app/models.py
+++ app/models.py
@@ -9,12 +9,21 @@ def slugify(title: str):
     return re.sub(pattern, '-', title).lower()
 
 
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
     slug = db.Column(db.String(140), unique=True)
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
+
+    # lazy means that we take BaseQuery instance
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
