a9644581769439e366b5563ff22d00c312f49e2a
miktuy <miktuy@mail.ru>
2020-05-13 00:41:23 +0300

    Added pagination

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -30,12 +30,22 @@ def create_post():
 @posts.route('/')
 def index():
     query = request.args.get('query')
+
+    page = request.args.get('page')
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
     if query:
-        posts = Post.query.filter(Post.title.contains(query) | Post.body.contains(query)).all()
+        posts = Post.query.filter(Post.title.contains(query) | Post.body.contains(query))  # .all()
     else:
         posts = Post.query.order_by(Post.created.desc())
 
-    return render_template('posts/index.html', posts=posts)
+    pages = posts.paginate(page=page, per_page=5)
+
+    return render_template('posts/index.html', pages=pages)
 
 
 @posts.route('/<slug>')
