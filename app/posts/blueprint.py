b5e20e6668bfaaffeec569ae5e31463b0f4a22ce
miktuy <miktuy@mail.ru>
2020-05-12 23:27:21 +0300

    Added post creation and searching

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,14 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
+from app import db
 from models import Post, Tag
+from .forms import PostForm
 
 
 posts = Blueprint(name='posts', import_name=__name__, template_folder='templates')
 
 
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            print('Something wrong')
+        return redirect(url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    query = request.args.get('query')
+    if query:
+        posts = Post.query.filter(Post.title.contains(query) | Post.body.contains(query)).all()
+    else:
+        posts = Post.query.order_by(Post.created.desc())
+
     return render_template('posts/index.html', posts=posts)
 
 
