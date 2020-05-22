4889f1ab425e87d956f0d26f66ef0da0ed6be9d8
miktuy <miktuy@mail.ru>
2020-05-23 01:02:21 +0300

    Added security

--- app/config.py
+++ app/config.py
@@ -3,3 +3,7 @@ class Configuration:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/blog_post'
     SECRET_KEY = 'something very secret'
+
+    ### Flask-security
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'bcrypt'
--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 import re
 from datetime import datetime
 
-from app import db
+from flask_security import UserMixin, RoleMixin
 
+from app import db
 
 def slugify(title: str):
     pattern = r'[^\w+]'
@@ -48,3 +49,26 @@ class Tag(db.Model):
 
     def __repr__(self):
         return f'{self.name}'
+
+
+# Flask security used:
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+                       )
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))  # TODO just for educ aims. In real life HAVE TO USE HASH!!! (flask bcrypt)
+    active = db.Column(db.Boolean)
+
+    roles = db.relationship(
+        'Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
