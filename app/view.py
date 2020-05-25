60522e0b6715203409fa4e9dd8f20c7660815fce
miktuy <miktuy@mail.ru>
2020-05-25 23:22:42 +0300

    404 page added some css style added

--- app/view.py
+++ app/view.py
@@ -8,6 +8,11 @@ def index():
     return render_template('index.html', name=name)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route('/admin')
 def open_admin():
     pass
